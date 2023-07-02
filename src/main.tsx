ba5483832652268712592f99934db824ce07aea8
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2023-07-02 11:15:39 -0300

    refactor: melhoria de estrutura

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import JogoDaMemoria from "./pages/JogoDaMemoria";
-import "./index.css";
+import "./assets/css/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import JogoDaMemoriaContext, {
   JogoDaMemoriaContextProvider,
